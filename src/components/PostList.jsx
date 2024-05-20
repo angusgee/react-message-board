d9f6fd641a593cfc09ccc03ba2c8a5977d7f9090
angusgee <maltawebdev@gmail.com>
2024-05-20 23:07:01 +0200

    Finish styling PostList component

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,10 +1,10 @@
 import Post from "./Post";
-import styles from "./PostList.module.css";
+import classes from "./PostList.module.css";
 
 function PostList() {
   return (
     <>
-      <ul>
+      <ul className={classes.posts}>
         <Post author="Gus" text="Thanks for checking out my site!" />
         <Post author="Ang" text="View my blog here!" />
         <Post author="Drew" text="Check out previous work here." />
