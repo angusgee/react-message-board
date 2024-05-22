b7399c88106c4f8708f6bc067148c4d41d7c9888
angusgee <maltawebdev@gmail.com>
2024-05-22 11:57:02 +0200

    add change body handler

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,9 +1,11 @@
 import Post from "./Post";
+import NewPost from "./NewPost";
 import classes from "./PostList.module.css";
 
 function PostList() {
   return (
     <>
+      <NewPost />
       <ul className={classes.posts}>
         <Post author="Gus" text="Thanks for checking out my site!" />
         <Post author="Ang" text="View my blog here!" />
