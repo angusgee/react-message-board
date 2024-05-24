4b23af654707fc2ec66690d92a2ebaf1f5a9724a
angusgee <maltawebdev@gmail.com>
2024-05-24 22:34:31 +0200

    Add submit functionality
    - move bodyChange and authorChange handlers to newPost component
    - add submitHandler which captures the entered values into an object called postData

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -5,30 +5,14 @@ import classes from "./PostList.module.css";
 import { useState } from "react";
 
 function PostList({ modalIsVisible, onHideModal }) {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
   return (
     <>
       {modalIsVisible && (
         <Modal onClose={onHideModal}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-            onCancel={onHideModal}
-          />
+          <NewPost onCancel={onHideModal} />
         </Modal>
       )}
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} text={enteredBody} />
         <Post author="Ang" text="View my blog here!" />
         <Post author="Drew" text="Check out previous work here." />
       </ul>
