9c469b393b409e40228d635142f51532c30bc64b
angusgee <maltawebdev@gmail.com>
2024-05-24 21:36:30 +0200

    Add main page header
    - add react icons lib
    - add main header component and stylesheet

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import PostList from "./components/PostList";
+import MainHeader from "./components/MainHeader";
 
 function App() {
   return (
     <main>
+      <MainHeader />
       <PostList />
     </main>
   );
